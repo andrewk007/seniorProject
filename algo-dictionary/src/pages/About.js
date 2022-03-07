a9582cefcdf78ce5edee011a175711c3d692eba0
Andrew Ku <andrewku@Andrews-MacBook-Pro-2.local>
2022-03-07 07:34:32 -0500

    derp

--- algo-dictionary/src/pages/About.js
+++ algo-dictionary/src/pages/About.js
@@ -1,36 +1,16 @@
 import React from 'react'
 import '../pages/About.css'
-import styled from 'styled-components';
 
-const Wrapper = styled.section`
-  display:flex;
-  background-color:white;
-  color:black;
-  width:auto;
-  height:auto;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-`;
 const summary = 'This website exists to help break down complex algorithms intuitively, allowing the user to manipulate parameters and visually break down the problem.';
 
 
-const algos = [
-    {
-        title:'BFS'
-},
-{
-    title: 'Fulkerson'
-}
-];
 
 function About() {
     return (
         <div className = 'about'>
             {summary}
-            <ul>
-                {algos.map(function(algo){
-                    return <Wrapper>{algo.title} </Wrapper>;
-                })}
-            </ul>
+                
+
         </div>
     );
 }
--- algo-dictionary/src/pages/Algorithms.js
+++ algo-dictionary/src/pages/Algorithms.js
@@ -1,13 +1,32 @@
 import React from 'react'
-import Canvas from '../components/Canvas'
 import '../pages/Algorithms.css'
 
+import styled from 'styled-components';
 
+const Wrapper = styled.section`
+  display:flex;
+  background-color:white;
+  color:black;
+  width:auto;
+  height:auto;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  
+`;
+const algos = [
+    {
+        title:'BFS'
+},
+{
+    title: 'Fulkerson'
+}
+];
 function Algorithms() {
     return (
         <div className = 'algorithms'>
             <h1> Algorithms</h1>
-            <Canvas/>
+            {algos.map(function(algo){
+                    return <Wrapper>{algo.title} </Wrapper>;
+                })}
    
 
         </div>
